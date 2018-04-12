03dcf63a65beefdb62738a8a59573b82eac7321d
Alexey Yakovenko <wakeroid@gmail.com>
2018-04-12 11:35:31 +0200

    added headerbar plugin

--- travis/upload.sh
+++ travis/upload.sh
@@ -1,5 +1,5 @@
 echo Decrtypting id_rsa...
-openssl aes-256-cbc -K $encrypted_cba67b5a8c55_key -iv $encrypted_cba67b5a8c55_iv -in travis/id_rsa.enc -out travis/id_rsa -d
+openssl aes-256-cbc -K $encrypted_cba67b5a8c55_key -iv $encrypted_cba67b5a8c55_iv -in travis/id_rsa.enc -out travis/id_rsa -d || exit 1
 eval "$(ssh-agent -s)"
 chmod 600 travis/id_rsa
 ssh-add travis/id_rsa || exit 1
@@ -10,13 +10,13 @@ OUTDIR=temp/output
 case "$TRAVIS_OS_NAME" in
     linux)
         echo Uploading linux artifacts...
-        rsync -a -e "$SSHOPTS" $OUTDIR/i686/*.zip waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/plugins/i686
-        rsync -a -e "$SSHOPTS" $OUTDIR/x86_64/*.zip waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/plugins/x86_64
+        rsync -a -e "$SSHOPTS" $OUTDIR/i686/*.zip waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/plugins/i686 || exit 1
+        rsync -a -e "$SSHOPTS" $OUTDIR/x86_64/*.zip waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/plugins/x86_64 || exit 1
 
         cat web/header.html >$OUTDIR/web/plugins.html
         cat web/plugins-head.html >>$OUTDIR/web/plugins.html
         cat $OUTDIR/web/plugins-content.html >>$OUTDIR/web/plugins.html
         cat web/footer.html >>$OUTDIR/web/plugins.html
-        rsync -a -e "$SSHOPTS" $OUTDIR/web/plugins.html waker,deadbeef@web.sourceforge.net:/home/groups/d/de/deadbeef/htdocs/
+        rsync -a -e "$SSHOPTS" $OUTDIR/web/plugins.html waker,deadbeef@web.sourceforge.net:/home/groups/d/de/deadbeef/htdocs/ || exit 1
     ;;
 esac
